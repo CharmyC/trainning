21ec6ba7c0510c49b5ce66f4ff5e2d6349a061dd
zhuanli.cheng <zhuanli.cheng@qunar.com>
2017-12-09 13:35:16 +0800

    add algorithm

--- src/main/java/algorithm/sort/QuickSort.java
+++ src/main/java/algorithm/sort/QuickSort.java
@@ -1,12 +1,4 @@
 package algorithm.sort;
 
-import java.util.List;
-
-/**
- * Created by zhuanli.cheng on 2017/11/2.
- */
-public class QuickSort<T> {
-    void quickSort(final List<T> array, int sort){
-
-    }
+public class QuickSort {
 }
